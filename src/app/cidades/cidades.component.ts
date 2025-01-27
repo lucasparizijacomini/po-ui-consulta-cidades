7fd2b5d690b6ce24b950c223274efc84b0bc1f10
Lucas Jacomini <lucasparizijacomini@gmail.com>
2025-01-27 20:14:35 -0300

    implementação da busca avancada

--- src/app/cidades/cidades.component.ts
+++ src/app/cidades/cidades.component.ts
@@ -1,25 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { PoBreadcrumb, PoNotificationService, PoPageFilter, PoPageModule, PoTableColumn, PoTableModule } from '@po-ui/ng-components';
+import {
+  PoBreadcrumb,
+  PoNotificationService,
+  PoPageFilter,
+  PoPageModule,
+  PoTableColumn,
+  PoTableModule,
+} from '@po-ui/ng-components';
 import { CidadesService } from '../shared/services/cidades.service';
-
+import {
+  BuscaAvancadaCidadeComponent,
+  CidadeBuscaAvancada,
+} from '../shared/components/busca-avancada-cidade/busca-avancada-cidade.component';
+import { FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cidades',
   templateUrl: './cidades.component.html',
-  imports: [PoPageModule, PoTableModule],
+  imports: [
+    CommonModule,
+    PoPageModule,
+    PoTableModule,
+    BuscaAvancadaCidadeComponent,
+  ],
   standalone: true,
   styleUrls: ['./cidades.component.css'],
 })
-export class CidadesComponent implements OnInit  {
+export class CidadesComponent implements OnInit {
+  @ViewChild('buscaAvancada') buscaAvancada!: BuscaAvancadaCidadeComponent;
+
   public readonly breadcrumb: PoBreadcrumb;
   public readonly filterSettings: PoPageFilter;
   public readonly cidadesColumns: Array<PoTableColumn>;
   public isLoading: boolean;
-  public items: Array<any>
-  private originalItems: Array<any>
+  public items: Array<any>;
+  private originalItems: Array<any>;
 
-  constructor(private router: Router, private cidadesService: CidadesService, private poNotification: PoNotificationService) {
+  constructor(
+    private router: Router,
+    private cidadesService: CidadesService,
+    private poNotification: PoNotificationService
+  ) {
     this.breadcrumb = this.buildBreadcrumb();
     this.filterSettings = this.buildFilter();
     this.cidadesColumns = this.buildColumns();
@@ -32,20 +55,24 @@ export class CidadesComponent implements OnInit  {
     this.buscarTodasCidades();
   }
 
-  private buscarTodasCidades(){
+  handleBuscar(form: CidadeBuscaAvancada) {
+    console.log('form', form);
+  }
+
+  private buscarTodasCidades() {
     this.isLoading = true;
     this.cidadesService.getAllCidades().subscribe({
-      next: (items: any)=>{
+      next: (items: any) => {
         this.originalItems = items;
         this.items = [...this.originalItems];
         this.isLoading = false;
       },
-      error: (err: any)=>{
-        console.error(err)
+      error: (err: any) => {
+        console.error(err);
         this.isLoading = false;
-        this.poNotification.error("Aconteceu um erro na busca das cidades");
-      }
-    })
+        this.poNotification.error('Aconteceu um erro na busca das cidades');
+      },
+    });
   }
 
   private buildBreadcrumb(): PoBreadcrumb {
@@ -70,7 +97,7 @@ export class CidadesComponent implements OnInit  {
     const term = searchTerm.trim().toLowerCase();
 
     if (term) {
-      this.items = this.originalItems.filter(item =>
+      this.items = this.originalItems.filter((item) =>
         item.nome.toLowerCase().includes(term)
       );
       this.isLoading = false;
@@ -80,7 +107,9 @@ export class CidadesComponent implements OnInit  {
     }
   }
 
-  private advancedFilterActionModal() {} // chamar o modal de busca avançada
+  private advancedFilterActionModal() {
+    this.buscaAvancada.openPageSlide();
+  }
 
   private beforeRedirect() {
     this.router.navigate(['/']);
@@ -90,6 +119,6 @@ export class CidadesComponent implements OnInit  {
     return [
       { property: 'id', label: 'Id', type: 'string' },
       { property: 'nome', label: 'Nome', type: 'string' },
-    ]
+    ];
   }
 }
